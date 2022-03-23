82b268dd9e9379057eb3dfc475376b1af6341f0d
Coelho7638 <77635373+Coelho7638@users.noreply.github.com>
2022-03-23 09:56:53 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,6 +8,72 @@ app.get("/", function (req, res) {
     res.send("Ola usuário"); 
 })
 
+const token = ["teste"]
+
+
+app.get('/api/consulta/cnpj/:q/:q2', async (req, res) => {
+	var isToken = token.includes(`${req.params.q2}`)
+	var tokencheck = isToken ? 's' : 'n'
+	if (tokencheck != 'n') {
+	cnpj = await axios(`https://www.receitaws.com.br/v1/cnpj/${req.params.q}`);
+	res.json(cnpj.data)
+} else {
+	res.status(200).send({
+		status: true,
+		message: 'server ok',
+		error: 'infelizmente você não possui o token para realizar a consulta, compre o token com o Markos > wa.me/5594991423691'
+	  });
+}
+})
+
+app.get('/api/consulta/nome/:q/:q2', async (req, res) => {
+	var isToken = token.includes(`${req.params.q2}`)
+	var tokencheck = isToken ? 's' : 'n'
+	if (tokencheck != 'n') {
+	nome = await axios.get(`https://api.i-find.dev/?token=b4ded580-a8a6-4d66-9f03-93a26426391d&nome=${req.params.q}`);
+		
+
+	/*let teks = `═════════════════════
+🕵️  *CONSULTA REALIZADA*  🕵️
+═════════════════════\n`
+for (let v of api.data) {
+                teks += `NOME: ${v.Nome}\n`
+                teks += `CPF: ${v.Cpf}\n`
+                teks += `SEXO: ${v.Genero}\n`
+                teks += `DATA DE NASCIMENTO: ${v.Nascimento}\n`
+                teks += `━━━━━━━━━━━━━━━━━━\n\n`
+}*/ 
+	res.json({
+	  'Dono': "@sla_slazinn", 
+	  'result': nome.data,
+	});
+} else {
+	res.status(200).send({
+		status: true,
+		message: 'server ok',
+		error: 'infelizmente você não possui o token para realizar a consulta, compre token com wa.me/5511966491483'
+	  });
+}
+})
+
+app.get('/api/consulta/telefone/:q/:q2', async (req, res) => {
+	var isToken = token.includes(`${req.params.q2}`)
+	var tokencheck = isToken ? 's' : 'n'
+	if (tokencheck != 'n') {
+	tel = await axios.get(`https://api.i-find.dev/?token=b4ded580-a8a6-4d66-9f03-93a26426391d&telDb=${req.params.q}`);
+	
+	res.json({
+	  'Dono': "@sla_slazinn",  
+	  'result': tel.data,
+	});
+} else {
+	res.status(200).send({
+		status: true,
+		message: 'server ok',
+		error: 'infelizmente você não possui o token para realizar a consulta, compre token com wa.me/5511966491483'
+	  });
+}
+})
 app.listen(port, ( ) => {
     console.info(`Aplicaçao rodando em http://localhost:${port}`);
 });
