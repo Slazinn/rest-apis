bd2b2b3159b13727357999c266716e5cd76c1148
Coelho7638 <77635373+Coelho7638@users.noreply.github.com>
2022-04-06 16:03:38 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -24,7 +24,7 @@ app.get("/", function (req, res) {
 
 const token = ["slazinnnn","loserzinn","texasdomina","Komi&san"]
 
-app.get('/api/consulta/cnpj/:q/:q2', async (req, res) => {
+/*app.get('/api/consulta/cnpj/:q/:q2', async (req, res) => {
 	var isToken = token.includes(`${req.params.q2}`)
 	var tokencheck = isToken ? 's' : 'n'
 	if (tokencheck != 'n') {
@@ -41,7 +41,7 @@ app.get('/api/consulta/cnpj/:q/:q2', async (req, res) => {
 
  //SISTEMA PUSSY
 
-/*  app.get('/api/nsfw/pussy&token=:q2', async (req, res) => {
+ app.get('/api/nsfw/pussy&token=:q2', async (req, res) => {
 	var isToken = token.includes(`${req.params.q2}`)
 	var tokencheck = isToken ? 's' : 'n'
 	if (tokencheck != 'n') {
